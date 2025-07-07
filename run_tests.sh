c20462a237a508c9695a3e95e5ac4925502cfa31
UserAnn7 <103679685+UserAnn7@users.noreply.github.com>
2025-07-07 14:58:02 +0200

    Updating run_tests file

--- run_tests.sh
+++ run_tests.sh
@@ -8,9 +8,3 @@ poetry run pytest -s --soft-asserts --html=report.html --self-contained-html --a
 
 #echo "Generating Allure report..."
 #allure generate allure-results -o allure-report --clean
-
-#echo "Opening HTML report"
-#open report.html
-
-#echo "Opening Allure results"
-#allure serve allure-results
